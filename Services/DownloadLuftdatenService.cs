b9eaf0a16d8262fbe083cb520597a825129d082a
Georgi Filipov <georgi.filipov@uplink.bg>
2018-02-22 14:24:34 +0200

    Implement download process.

--- Services/DownloadLuftdatenService.cs
+++ Services/DownloadLuftdatenService.cs
@@ -1,14 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using DownloadAirInfo.Models.Download;
+using HtmlAgilityPack;
 
 namespace DownloadAirInfo.Services
 {
     public class DownloadLuftdatenService : IDownloadService
     {
-        public Task DownloadAsync(Configuration configuration)
+        public async Task DownloadAsync(Configuration configuration)
         {
-            throw new NotImplementedException();
+            if(!Directory.Exists(configuration.ArchiveDirectory))
+            {
+                Directory.CreateDirectory(configuration.ArchiveDirectory);
+            }
+
+            using (WebClient client = new WebClient())
+            {
+                var endpointUri = new Uri(configuration.Endpoint);
+
+                var endpointHtml = await client.DownloadStringTaskAsync(endpointUri);
+
+                HtmlDocument document = new HtmlDocument();
+
+                document.LoadHtml(endpointHtml);
+
+                var links = document.DocumentNode.SelectNodes("//a");
+                foreach (DateTime day in EachDay(configuration.StartDate, configuration.EndDate))
+                {
+                    var link = document.DocumentNode.SelectSingleNode("//a[starts-with(@href, '" + day.ToString("yyyy-MM-dd") + "')]");
+                    if (link != null)
+                    {
+                        var sensorsUri = new Uri(endpointUri, link.GetAttributeValue("href", string.Empty));
+                        var sensorsHtml = await client.DownloadStringTaskAsync(sensorsUri);
+
+                        HtmlDocument sensorsPage = new HtmlDocument();
+                        sensorsPage.LoadHtml(sensorsHtml);
+
+                        var sensorsFileLink = sensorsPage.DocumentNode.SelectNodes("//a[starts-with(@href, '" + day.ToString("yyyy-MM-dd") + "')]");
+                        foreach(var sensorFileLink in sensorsFileLink)
+                        {
+                            var fileName = sensorFileLink.GetAttributeValue("href", string.Empty);
+                            var sensorFileUri = new Uri(sensorsUri, fileName);
+                            await client.DownloadFileTaskAsync(sensorFileUri, Path.Combine(configuration.ArchiveDirectory, fileName));
+
+                            Thread.Sleep(configuration.SleepTime);
+                        }
+                    }
+
+                    Thread.Sleep(configuration.SleepTime);
+                }
+            }
+        }
+
+        private IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+                yield return day;
         }
     }
 }
